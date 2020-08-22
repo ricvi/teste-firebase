05a9856d05f73a9febbd78f394861adfcd93d850
ryananjasmara <ryananjasmara1997@gmail.com>
2020-08-22 16:30:03 +0700

    integrating firebase -> integrate firebase crashlytic

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React, {useEffect} from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
-import {firebaseEventTracking} from './src/services/index';
+import {
+  firebaseEventTracking,
+  firebaseTriggerCrash,
+} from './src/services/index';
 
 const App = () => {
   /** Lifecycle Section */
 
   // Did Mount
   useEffect(() => {
-    console.log('hello');
+    console.log('App.js Did Mount');
   }, []);
 
   /** Functional Section */
@@ -17,13 +20,37 @@ const App = () => {
     firebaseEventTracking('button_tapped', {buttonName: 'tap me'});
   };
 
-  return (
-    <View style={styles.container}>
+  // onPress "Crash Me" Button
+  const onPressCrashMeButton = () => {
+    firebaseTriggerCrash();
+  };
+
+  /** Render Section */
+
+  const renderTapMeButton = () => {
+    return (
       <TouchableOpacity
         style={styles.customButton}
         onPress={() => onPressTapMeButton()}>
-        <Text>Tap Me</Text>
+        <Text style={styles.customButtonText}>Tap Me</Text>
       </TouchableOpacity>
+    );
+  };
+
+  const renderCrashMeButton = () => {
+    return (
+      <TouchableOpacity
+        style={styles.customButton}
+        onPress={() => onPressCrashMeButton()}>
+        <Text style={styles.customButtonText}>Crash Me</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      {renderTapMeButton()}
+      {renderCrashMeButton()}
     </View>
   );
 };
@@ -37,10 +64,14 @@ const styles = StyleSheet.create({
   customButton: {
     width: 100,
     padding: 12,
-    borderColor: 'blue',
+    borderColor: 'green',
     borderWidth: 2,
     borderRadius: 5,
     alignItems: 'center',
+    marginBottom: 10,
+  },
+  customButtonText: {
+    color: 'green',
   },
 });
 
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,12 @@
 import analytics from '@react-native-firebase/analytics';
+import crashlytics from '@react-native-firebase/crashlytics';
 
 function firebaseEventTracking(eventName, eventData) {
   analytics().logEvent(eventName, eventData);
 }
 
-export {firebaseEventTracking};
+function firebaseTriggerCrash() {
+  crashlytics().crash();
+}
+
+export {firebaseEventTracking, firebaseTriggerCrash};
