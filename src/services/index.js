daf6e2ce2cc78e8e6214d110ede92ffdfdf9a9b4
ryananjasmara <ryananjasmara1997@gmail.com>
2020-08-31 11:00:45 +0700

    firebase cloud messaging -> integrate fcm with push notification (done only for foreground)

--- src/services/index.js
+++ src/services/index.js
@@ -64,9 +64,9 @@ async function firebaseCloudMessagingGetToken() {
   return await messaging().getToken();
 }
 
-function firebaseCloudMessagingForeground() {
+function firebaseCloudMessagingForeground(notification) {
   const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-    Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    notification.localNotif(remoteMessage);
   });
 
   return unsubscribe;
