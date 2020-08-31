46ce3b8a60c36035b82e415ac92bcb05582185a8
ryananjasmara <ryananjasmara1997@gmail.com>
2020-08-31 13:25:53 +0700

    integrating firebase -> create index for firebase services

---
+++ src/services/Firebase/index.js
@@ -0,0 +1,7 @@
+import CloudMessaging from './CloudMessaging';
+import Crashlytic from './Crashlytic';
+import Event from './Event';
+import InAppMessaging from './InAppMessaging';
+import RemoteConfig from './RemoteConfig';
+
+export {CloudMessaging, Crashlytic, Event, InAppMessaging, RemoteConfig};
